1c2812207325ff2900592d788938905d655e9fbb
bonnie <arlolocos@gmail.com>
2022-08-10 03:27:32 -0400

    rolls and shakes

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Die from './Die.jsx';
 
 const App = () => {
+  const [rolling, setRolling] = useState(false);
+  const [die1, setDie1] = useState(1);
+  const [die2, setDie2] = useState(1);
+
+  const roll = () => {
+    setRolling(true);
+    let rand1 = Math.floor(Math.random() * 6);
+    let rand2 = Math.floor(Math.random() * 6);
+    setDie1(rand1);
+    setDie2(rand2);
+  }
+
+  setTimeout(() => {
+    setRolling(false);
+  }, 4000)
+
   return (
-    <div>
-      App
+    <div className='main'>
+      <div className='app-die-container'>
+      <Die num={die1} shake={rolling}/> <Die num={die2} shake={rolling}/>
+      </div>
+      <button
+        onClick={roll}
+        className='app-button'
+        disabled={rolling}
+      >
+        {rolling ? 'rolling...' : 'Roll Dice'}
+      </button>
     </div>
   )
 }
---
+++ client/src/Components/Die.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { BsDice1 } from "react-icons/bs";
+import { BsDice2 } from "react-icons/bs";
+import { BsDice3 } from "react-icons/bs";
+import { BsDice4 } from "react-icons/bs";
+import { BsDice5 } from "react-icons/bs";
+import { BsDice6 } from "react-icons/bs";
+import { IoDiceOutline } from "react-icons/io5";
+
+const Die = (props) => {
+
+  let Num;
+
+  switch (props.num) {
+    case 1:
+      Num = BsDice1;
+      break;
+    case 2:
+      Num = BsDice2;
+      break;
+    case 3:
+      Num = BsDice3;
+      break;
+    case 4:
+      Num = BsDice4;
+      break;
+    case 5:
+      Num = BsDice5;
+      break;
+    case 6:
+      Num = BsDice6;
+      break;
+    default:
+      Num = BsDice6;
+  }
+
+  let shake = props.shake ? 'shake' : '';
+
+  return (
+    <span className='die-span' >
+      <Num className={`die ${props.shake ? 'shaking' : ''}`}/>
+    </span>
+  )
+}
+
+export default Die;
